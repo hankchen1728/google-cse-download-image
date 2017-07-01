8e600666caf2ef02bd81bab4e8a8c040a1a29f6a
hankchen1728 <hankchen1728@gmail.com>
2017-07-02 01:38:01 +0800

    Add files via upload

---
+++ google_api_image_download.py
@@ -0,0 +1,47 @@
+from apiclient.discovery import build
+import requests
+import os
+import json
+
+Search_engine = input('Enter the search engine to use in your request:')
+api_key = input('Enter your API key:')
+search = input('Enter what you want to search:')
+DIR = '/Users/hankchen/downloads/'
+DIR = os.path.join(DIR,search)
+if not os.path.exists(DIR):
+    os.mkdir(DIR)
+
+def build_request(startindex,low_range):
+    service = build("customsearch", "v1",developerKey=api_key)
+    res = service.cse().list(
+        q = search,
+        cx = Search_engine,
+        num = 10,
+        start = startindex,
+        searchType='image',
+        safe= 'off',
+        lowRange=low_range,
+        highRange=low_range+100
+    ).execute()
+    return res
+
+for x in range(1,1000,10):
+    print("這是第",x,"~",x+9,"結果")
+    StartIndex = x%100
+    LowRange = (x//100)*100
+    res = build_request(StartIndex,LowRange)
+    count = 0
+    for item in res['items']:
+        index = x + count
+        #print(item['link'])
+        link = item['link']
+        reqimg = requests.get(link,stream=True,headers=header)
+        imgdata = reqimg.content
+        filename = search+str(index)+'.jpg'
+        #以返回的二進位制數據創建圖片
+        f = open(os.path.join(DIR , filename), 'wb')
+        f.write(imgdata)
+        f.close
+        del reqimg
+        print('第',index,'張',search,'圖片下載完成')
+        count += 1
