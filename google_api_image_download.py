7f381c9b6d794932e152547a909c185f2d84c748
hankchen1728 <hankchen1728@gmail.com>
2017-07-02 16:13:22 +0800

    Update google_api_image_download.py

--- google_api_image_download.py
+++ google_api_image_download.py
@@ -12,6 +12,8 @@ DIR = os.path.join(DIR,search)
 if not os.path.exists(DIR):
     os.mkdir(DIR)
 
+header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/602.4.8 (KHTML, like Gecko) Version/10.0.3 Safari/602.4.8'}
+
 def build_request(startindex,low_range):
     service = build("customsearch", "v1",developerKey=api_key)
     res = service.cse().list(
